e4cf8c5e7e3708820acc20d998da5a776a26dc60
Nirav Varma <nirav.iitk@gmail.com>
2021-03-18 20:36:04 +0100

    added little description

--- 3-dynamic-object-keys/app.js
+++ 3-dynamic-object-keys/app.js
@@ -11,3 +11,25 @@ const newPerson = person.map(function (item) {
   return item;
 });
 console.log(newPerson);
+
+//dynamic object keys are always used using Square bracket notation
+const key = 'loading';
+const status = 'status';
+const state = {
+  [key]: true, //here key refers to loading
+  status: 'pending', //her status refers to internal key
+};
+console.log(state);
+state[key] = false; // same as state.loading
+state.status = 'finished';
+state[status] = 'working'; //key matches, thus, object is updated
+console.log(state);
+
+//furthermore a function can be written which can change the behavior of the objects
+function changeState(key, val) {
+  state[key] = val;
+}
+changeState('loading', true); //either directly by key name
+changeState(key, false); //either by reference
+changeState('new', true); // adding new property
+console.log(state);
